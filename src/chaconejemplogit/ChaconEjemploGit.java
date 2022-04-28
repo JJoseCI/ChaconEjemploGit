9f9cc5c89397702b2fba293c7aa0f93ac11e0fb3
damt106 <damt106@36-12.aglinformatica.es>
2022-04-28 18:48:36 +0200

    Version 1

---
+++ src/chaconejemplogit/ChaconEjemploGit.java
@@ -0,0 +1,17 @@
+package chaconejemplogit;
+
+/**
+ *
+ * @author damt106
+ */
+public class ChaconEjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Jose Maria Chacon Inga");
+        System.out.println("Version 1");
+    }
+
+}
